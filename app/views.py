90f60e71531530cc90121fc9b91f08b024f72345
briancurtin <brian@python.org>
2017-06-22 15:17:07 -0400

    Better layout

--- app/views.py
+++ app/views.py
@@ -3,6 +3,16 @@ from flask import render_template
 from app import app
 
 @app.route('/')
-@app.route('/<name>')
-def render(name="index"):
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/photography/<name>')
+def photography(name="baseball"):
     return render_template("%s.html" % name)
